cde17204e24d30b200911842144e6844abb34349
Thường Tôn <thuonglovevietnam@gmail.com>
2020-02-27 16:01:43 +0700

    some message

--- src/components/Project.js
+++ src/components/Project.js
@@ -37,8 +37,24 @@ export default class Project extends Component {
             </div>
           </div>
         </div>
+        <div className="col-sm-6">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Trang web bán hàng đơn giản chưa chi tiết & authenticated</h5>
+              <p className="card-text">sẽ cải tiến trong tương lai chỉ là cơ bản, sử dụng html/css/react/express/mongodb</p>
+              <p>admin account demo:</p>
+              <ul className="card-text">
+                <li>email: z@gmail.com</li>
+                <li>pass: thuong123</li>
+              </ul>
+              <p>admin truy cập vào các trang /buy . /buyi ./add</p>
+              <a rel='noopener noreferrer' target="_blank" href="https://product-frontend-thuong.herokuapp.com" className="btn btn-primary">Go Project</a>
+            </div>
+          </div>
+        </div>
       </div>
       </div>
+      
           </div>
         )
     }
